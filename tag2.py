241585b8f5d5b699765d07e4981229ba076ec039
Dimitri <dimiknoll210@gmail.com>
2026-07-25 11:40:12 +0200

    Tag 2: Eingabe und Rechnungen

---
+++ tag2.py
@@ -0,0 +1,7 @@
+zahl1 = float(input("Erste Zahl: "))
+zahl2 = float(input("Zweite Zahl: "))
+
+print(f"Summe: {zahl1 + zahl2}")
+print(f"Differenz: {zahl1 - zahl2}")
+print(f"Produkt: {zahl1 * zahl2}")
+print(f"Quotient: {zahl1 / zahl2}")
