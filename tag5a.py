c25c6a7c21a253ff862cc34ec2d5c7b29a16a23f
Dimitri <dimiknoll210@gmail.com>
2026-07-27 20:47:05 +0200

    Tag 5: Listen

---
+++ tag5a.py
@@ -0,0 +1,6 @@
+Brands = ["Nike", "Adidas", "Puma"]
+Filme = ["Matrix", "Inception", "Interstellar"]
+
+
+for brand in Filme:
+    print(brand)
---
+++ tag5b.py
@@ -0,0 +1,9 @@
+
+Filme = ["Matrix", "Inception", "Interstellar"]
+
+
+neuer_film = input("Gib einen neuen Film ein: ")
+Filme.append(neuer_film)
+
+for film in Filme:
+    print(film)
